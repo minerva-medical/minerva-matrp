cc89cc6687b9d46ed8ce2ee0d23a8a3fea00f5e8
guanhongl <guanh.sl23@gmail.com>
2021-09-19 19:42:38 -1000

    re-add dropdown collections

--- app/imports/api/drug/DrugCollection.js
+++ app/imports/api/drug/DrugCollection.js
@@ -4,6 +4,7 @@ import SimpleSchema from 'simpl-schema';
 // import { _ } from 'meteor/underscore';
 // import { Roles } from 'meteor/alanning:roles';
 import BaseCollection from '../base/BaseCollection';
+import { ROLE } from '../role/Role';
 
 export const drugPublications = {
   drug: 'Drug',
@@ -76,6 +77,15 @@ class DrugCollection extends BaseCollection {
     return null;
   }
 
+  /**
+   * Default implementation of assertValidRoleForMethod. Asserts that userId is logged in as an Admin or User.
+   * This is used in the define, update, and removeIt Meteor methods associated with each class.
+   * @param userId The userId of the logged in user. Can be null or undefined
+   * @throws { Meteor.Error } If there is no logged in user, or the user is not an Admin or User.
+   */
+  assertValidRoleForMethod(userId) {
+    this.assertRole(userId, [ROLE.ADMIN, ROLE.USER]);
+  }
 }
 
 /**
--- app/imports/api/lotId/LotIdCollection.js
+++ app/imports/api/lotId/LotIdCollection.js
@@ -4,6 +4,7 @@ import SimpleSchema from 'simpl-schema';
 // import { _ } from 'meteor/underscore';
 // import { Roles } from 'meteor/alanning:roles';
 import BaseCollection from '../base/BaseCollection';
+import { ROLE } from '../role/Role';
 
 export const lotIdPublications = {
   lotId: 'LotId',
@@ -93,6 +94,15 @@ class LotIdCollection extends BaseCollection {
     return null;
   }
 
+  /**
+   * Default implementation of assertValidRoleForMethod. Asserts that userId is logged in as an Admin or User.
+   * This is used in the define, update, and removeIt Meteor methods associated with each class.
+   * @param userId The userId of the logged in user. Can be null or undefined
+   * @throws { Meteor.Error } If there is no logged in user, or the user is not an Admin or User.
+   */
+  assertValidRoleForMethod(userId) {
+    this.assertRole(userId, [ROLE.ADMIN, ROLE.USER]);
+  }
 }
 
 /**
