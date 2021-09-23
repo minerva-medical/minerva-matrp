34bad66b3d1679f03ef82381ef87897e84b27211
Glen Larita <glarita@hawaii.edu>
2021-09-22 22:35:03 -1000

    added more to test component

--- app/imports/ui/components/DrugPage.jsx
+++ app/imports/ui/components/DrugPage.jsx
@@ -12,14 +12,6 @@ const Drug = () => {
   const handleOpen = () => setState({ open: true });
   const { open } = state;
 
-  const drugBox = {
-    backgroundColor: 'white',
-    borderRadius: '15px',
-    padding: '20px',
-    paddingLeft: '50px',
-    paddingRight: '50px',
-    margin: '10px 135px 10px 135px',
-  };
   const notes = {
     backgroundColor: '#DCDED1',
     borderRadius: '15px',
@@ -41,65 +33,63 @@ const Drug = () => {
         <Portal onClose={handleClose} open={open}>
           <Segment
             style={{
-              left: '10%',
+              left: '8%',
               position: 'fixed',
               top: '15%',
               zIndex: 1000,
+              backgroundColor: 'white',
+              borderRadius: '15px',
+              paddingBottom: '30px',
+              paddingLeft: '50px',
+              paddingRight: '70px',
             }}
           >
             <Container>
-              <Header as="h2" textAlign="center">Drug Page</Header>
-              <Segment style={drugBox} size='medium'>
-                <Grid container divided columns='equal' stackable textAlign='justified'>
-                  <GridColumn>
-                    <ItemGroup relaxed>
-                      <Item>
-                        <ItemContent>
-                          <Header as='h2'> Aspirin 81 mg</Header>
-                          <ItemMeta>Nonsteroidal anti-inflammatory drug (NSAID)</ItemMeta>
-                          <ItemDescription>
-                            <List size='large'>
-                              <ListItem>Brand: Bayer</ListItem>
-                              <ListItem>Lot Number: 123456</ListItem>
-                              <ListItem>Expiration Date: 09/16/2021</ListItem>
-                              <ListItem>Quantity: 30 tabs</ListItem>
-                              <ListItem>Supply: 300</ListItem>
-                              <ListItem>Storage Location: Case 4</ListItem>
-                              <ListItem>Received: Purchased</ListItem> <br/>
-                            </List>
-                          </ItemDescription>
-                          <ItemExtra>
-                            <Button size='medium'>Edit</Button>
-                          </ItemExtra>
-                        </ItemContent>
-                      </Item>
-                    </ItemGroup>
-                  </GridColumn>
-                  <GridColumn>
-                    <Segment style={notes}>
-                      <Container fluid>
-                        <ItemGroup>
-                          <Item>
-                            <ItemContent>
-                              <Header as='h3'>Notes</Header>
-                              <ItemDescription>To be prescribed to patients that are at risk for myocardial infarction or
-                                stroke. Prescribed as take 1 tab daily.
-                              </ItemDescription>
-                            </ItemContent>
-                          </Item>
-                        </ItemGroup>
-                      </Container>
-                    </Segment>
-                  </GridColumn>
-                </Grid>
-              </Segment>
-            </Container>
+              <Header as="h2" textAlign="center" style={{ paddingBottom: '20px', paddingTop: '20px' }}>Drug</Header>
 
-            <Button
-              content='Close Portal'
-              negative
-              onClick={handleClose}
-            />
+              <Grid container divided columns='equal' stackable textAlign='justified'>
+                <GridColumn>
+                  <ItemGroup relaxed>
+                    <Item>
+                      <ItemContent>
+                        <Header as='h2'> Aspirin 81 mg</Header>
+                        <ItemMeta>Nonsteroidal anti-inflammatory drug (NSAID)</ItemMeta>
+                        <ItemDescription>
+                          <List size='large'>
+                            <ListItem>Brand: Bayer</ListItem>
+                            <ListItem>Lot Number: 123456</ListItem>
+                            <ListItem>Expiration Date: 09/16/2021</ListItem>
+                            <ListItem>Quantity: 30 tabs</ListItem>
+                            <ListItem>Supply: 300</ListItem>
+                            <ListItem>Storage Location: Case 4</ListItem>
+                            <ListItem>Received: Purchased</ListItem> <br/>
+                          </List>
+                        </ItemDescription>
+                        <ItemExtra>
+                          <Button size='medium'>Edit</Button>
+                        </ItemExtra>
+                      </ItemContent>
+                    </Item>
+                  </ItemGroup>
+                </GridColumn>
+                <GridColumn>
+                  <Segment style={notes}>
+                    <Container fluid>
+                      <ItemGroup>
+                        <Item>
+                          <ItemContent>
+                            <Header as='h3'>Notes</Header>
+                            <ItemDescription>To be prescribed to patients that are at risk for myocardial infarction or
+                                stroke. Prescribed as take 1 tab daily.
+                            </ItemDescription>
+                          </ItemContent>
+                        </Item>
+                      </ItemGroup>
+                    </Container>
+                  </Segment>
+                </GridColumn>
+              </Grid>
+            </Container>
           </Segment>
         </Portal>
       </Grid.Column>
