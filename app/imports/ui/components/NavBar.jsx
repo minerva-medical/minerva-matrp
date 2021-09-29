4da48355edcf115347ef948b34e88a2aa5988b16
Glen Larita <glarita@hawaii.edu>
2021-09-28 22:23:54 -1000

    Updates paths of user when logging in and Minerva in navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = ({ currentUser }) => {
   const menuStyle = { marginBottom: '30px', backgroundColor: '#1D3E66', backgroundSize: 'content' };
   return (
     <Menu style={menuStyle} attached="top" borderless inverted>
-      <Menu.Item id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} activeClassName="" exact to="/">
+      <Menu.Item id={COMPONENT_IDS.NAVBAR_LANDING_PAGE} as={NavLink} activeClassName="" exact to="/about">
         <Header inverted as='h1'>Minerva</Header>
       </Menu.Item>,
       <Menu.Item id={COMPONENT_IDS.NAVBAR_ABOUT_US} as={NavLink} activeClassName="active" exact to="/about" key='list'>
