9593bca43937446171b889874a37a4f3901d499b
alyssandra-cabading <alyssand@hawaii.edu>
2021-10-02 15:08:06 -1000

    Update DispenseLog.jsx

--- app/imports/ui/pages/DispenseLog.jsx
+++ app/imports/ui/pages/DispenseLog.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Container, Table, Segment, Divider, Dropdown, Pagination, Grid } from 'semantic-ui-react';
+import { Header, Container, Table, Segment, Divider, Dropdown, Pagination, Grid, Icon, Form } from 'semantic-ui-react';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
 /** Renders the Page for Dispensing Inventory. */
@@ -21,23 +21,46 @@ const reason = [
   { key: '2', value: 'Expired', text: 'Expired' },
   { key: '3', value: 'Broken/Contaminated', text: 'Broken/Contaminated' },
 ];
-  /** Render the form. */
+
+const inventory = [
+  { key: '0', value: 'All', text: 'All' },
+  { key: '1', value: 'Medication', text: 'Medication' },
+  { key: '2', value: 'Vaccination', text: 'Vaccination' },
+  { key: '3', value: 'Patient Supplies', text: 'Patient Supplies' },
+  { key: '4', value: 'Lab Testing Supplies', text: 'Lab Testing Supplies' },
+];
+
+/** Render the form. */
 const DispenseLog = () => (
   <Container id={PAGE_IDS.DISPENSE_LOG}>
     <Segment>
       <Header as="h2">
         <Header.Content>
-          History Dispense Log
+            History Dispense Log
           <Header.Subheader>
-            <i>Below is a history log of dispensed inventories.</i>
+            Use the search filter to check for a specific drug or click on the table header to sort the column.
           </Header.Subheader>
         </Header.Content>
       </Header>
       <Divider/>
       <Grid divided columns="equal">
+        <Grid.Row>
+          <Grid.Column>
+            <Form>
+              <Form.Field style={{ marginBottom: '1em' }}>
+                <Form.Input floated={'right'}
+                  placeholder={'Enter a filter.'}
+                  name={'filter'}
+                  label={'Filter'}
+                  icon={'search'}
+                />
+              </Form.Field>
+            </Form>
+          </Grid.Column>
+        </Grid.Row>
         <Grid.Row textAlign='center'>
           <Grid.Column>
-                  Sort By: {' '}
+              Sort By: {' '}
             <Dropdown
               inline={true}
               options={sort}
@@ -45,7 +68,7 @@ const DispenseLog = () => (
             />
           </Grid.Column>
           <Grid.Column>
-                  Reason for Dispense: {' '}
+              Dispense Type: {' '}
             <Dropdown
               inline={true}
               options={reason}
@@ -53,63 +76,64 @@ const DispenseLog = () => (
             />
           </Grid.Column>
           <Grid.Column>
-            Records per page:{' '}
+            Inventory Type: {' '}
+            <Dropdown
+              inline={true}
+              options={inventory}
+              defaultValue={'All'}
+            />
+          </Grid.Column>
+          <Grid.Column>
+              Records per page:{' '}
             <Dropdown
               inline={true}
               options={limitOptions}
               defaultValue={'10'}
             />
-            Total count: {'200'}
+              Total count: {'200'}
           </Grid.Column>
         </Grid.Row>
       </Grid>
       <Divider/>
-      <Table celled selectable sortable compact>
+      <Table celled selectable sortable compact columns={10}>
         <Table.Header>
-          <Table.Row inverted color="blue">
-            <Table.HeaderCell
-              width={1}
-            >
-                    Date : Time
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              width={2}
-            >
-                    Reason for Dispense
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              width={3}
-            >
-                    Medicine
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              width={1}
-            >
-                    Brand
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              width={1}
-            >
-                    Quantity
-            </Table.HeaderCell>
-            <Table.HeaderCell
-              width={3}
-            >
-                    Dispensed by
-            </Table.HeaderCell>
+          <Table.Row>
+            <Table.HeaderCell>Date & Time</Table.HeaderCell>
+            <Table.HeaderCell>Dispense Type</Table.HeaderCell>
+            <Table.HeaderCell>Patient Number</Table.HeaderCell>
+            <Table.HeaderCell>Medication Type</Table.HeaderCell>
+            <Table.HeaderCell>Medication Name</Table.HeaderCell>
+            <Table.HeaderCell>LotID</Table.HeaderCell>
+            <Table.HeaderCell>Quantity</Table.HeaderCell>
+            <Table.HeaderCell>Dispensed by</Table.HeaderCell>
+            <Table.HeaderCell>Detailed Notes</Table.HeaderCell>
           </Table.Row>
           <Table.Row>
             <Table.Cell>2021-09-13 (12:25:30)</Table.Cell>
             <Table.Cell>Patient Use</Table.Cell>
-            <Table.Cell>Cold and Flu Syrup</Table.Cell>
-            <Table.Cell>NyQuil</Table.Cell>
-            <Table.Cell>355 mL</Table.Cell>
+            <Table.Cell>123456</Table.Cell>
+            <Table.Cell>Analgesics/Anti-inflammatory</Table.Cell>
+            <Table.Cell>Acetaminophen 500 mg Caps</Table.Cell>
+            <Table.Cell>76543A21</Table.Cell>
+            <Table.Cell>60 tabs</Table.Cell>
             <Table.Cell>johndoe@hawaii.edu</Table.Cell>
+            <Table.Cell><Icon name="info circle"/>See details</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>2021-09-13 (12:25:30)</Table.Cell>
+            <Table.Cell>Patient Use</Table.Cell>
+            <Table.Cell>123456</Table.Cell>
+            <Table.Cell>Analgesics/Anti-inflammatory</Table.Cell>
+            <Table.Cell>Acetaminophen 500 mg Caps</Table.Cell>
+            <Table.Cell>76543A21</Table.Cell>
+            <Table.Cell>60 tabs</Table.Cell>
+            <Table.Cell>johndoe@hawaii.edu</Table.Cell>
+            <Table.Cell><Icon name="info circle"/>See details</Table.Cell>
           </Table.Row>
         </Table.Header>
         <Table.Footer>
           <Table.Row>
-            <Table.HeaderCell colSpan="8">
+            <Table.HeaderCell colSpan="10">
               <Pagination
                 totalPages={10}
                 activePage={1}
