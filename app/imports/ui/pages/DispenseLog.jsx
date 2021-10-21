49f11037c49193acdce0ea36f0b0c6c97ef08b7a
alyssandra-cabading <alyssand@hawaii.edu>
2021-10-20 20:27:29 -1000

    Update DispenseLog.jsx

--- app/imports/ui/pages/DispenseLog.jsx
+++ app/imports/ui/pages/DispenseLog.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { Header, Container, Table, Segment, Divider, Dropdown, Pagination, Grid, Loader, Input } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 import { Medications } from '../../api/medication/MedicationCollection';
 import { Historicals } from '../../api/historical/HistoricalCollection';
 import { DrugTypes } from '../../api/drugType/DrugTypeCollection';
 import DispenseLogRow from '../components/DispenseLogRow';
 import { distinct } from '../utilities/Functions';
+
+const getOptions = (arr) => {
+  const options = arr.map(elem => ({ key: elem, text: elem, value: elem }));
+  options.unshift({ key: '0', value: 'All', text: 'All' });
+  return options;
+};
 
 /** Renders the Page for Dispensing Inventory. */
 const limitOptions = [
@@ -27,105 +34,120 @@ const reason = [
   { key: '2', value: 'Expired', text: 'Expired' },
   { key: '3', value: 'Broken/Contaminated', text: 'Broken/Contaminated' },
 ];
+const exampleReducer = (state, action) => {
+  switch (action.type) {
+  case 'CHANGE_SORT':
+    if (state.column === action.column) {
+      return {
+        ...state,
+        data: state.data.slice().reverse(),
+        direction:
+              state.direction === 'ascending' ? 'descending' : 'ascending',
+      };
+    }
 
-const inventory = [
-  { key: '0', value: 'All', text: 'All' },
-  { key: '1', value: 'Medication', text: 'Medication' },
-  { key: '2', value: 'Vaccination', text: 'Vaccination' },
-  { key: '3', value: 'Patient Supplies', text: 'Patient Supplies' },
-  { key: '4', value: 'Lab Testing Supplies', text: 'Lab Testing Supplies' },
-];
+    return {
+      column: action.column,
+      data: _.sortBy(state.data, [action.column]),
+      direction: 'ascending',
+    };
+  default:
+    throw new Error();
+  }
+};
 
 /** Render the form. */
-const DispenseLog = (props) => {
-  if (props.ready) {
+const DispenseLog = ({ ready, historicals, drugTypes }) => {
+  if (ready) {
+    const gridAlign = {
+      textAlign: 'center',
+    };
     return (
       <div>
         <Container id='dispense-log'>
-          <Grid id='dispense-log-grid' centered>
-            <Segment>
-              <Header as="h2">
-                <Header.Content>
+          <Segment>
+            <Header as="h2">
+              <Header.Content>
                   History Dispense Log
-                  <Header.Subheader>
-                    <i>Use the search filter to look for a specific Patient Number or click on the table header to sort the
+                <Header.Subheader>
+                  <i>Use the search filter to look for a specific Patient Number or click on the table header to sort the
                       column.</i>
-                  </Header.Subheader>
-                </Header.Content>
-              </Header>
-              <Divider/>
-              <Grid>
-                <Grid.Column width={4}>
-                  <Input placeholder='Filter by patient number...' icon='search'/>
-                </Grid.Column>
-              </Grid>
-              <Divider/>
-              <Grid divided columns="equal">
-                <Grid.Row textAlign='center'>
-                  <Grid.Column>
+                </Header.Subheader>
+              </Header.Content>
+            </Header>
+            <Divider/>
+            <Grid>
+              <Grid.Column width={4}>
+                <Input placeholder='Filter by patient number...' icon='search'/>
+              </Grid.Column>
+            </Grid>
+            <Divider/>
+            <Grid divided columns="equal">
+              <Grid.Row style={gridAlign}>
+                <Grid.Column>
                     Sort By: {' '}
-                    <Dropdown
-                      inline={true}
-                      options={sort}
-                      defaultValue={'Most Recent'}
-                    />
-                  </Grid.Column>
-                  <Grid.Column>
+                  <Dropdown
+                    inline={true}
+                    options={sort}
+                    defaultValue={'Most Recent'}
+                  />
+                </Grid.Column>
+                <Grid.Column>
+                    Medication Type: {' '}
+                  <Dropdown
+                    inline
+                    options={getOptions(drugTypes)}
+                    search
+                    defaultValue={'All'}
+                  />
+                </Grid.Column>
+                <Grid.Column>
                     Dispense Type: {' '}
-                    <Dropdown
-                      inline={true}
-                      options={reason}
-                      defaultValue={'All'}
-                    />
-                  </Grid.Column>
-                  <Grid.Column>
-                    Inventory Type: {' '}
-                    <Dropdown
-                      inline={true}
-                      options={inventory}
-                      defaultValue={'All'}
-                    />
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-              <Divider/>
-              <div>
+                  <Dropdown
+                    inline={true}
+                    options={reason}
+                    defaultValue={'All'}
+                  />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            <Divider/>
+            <div>
                 Records per page:{' '}
-                <Dropdown
-                  inline={true}
-                  options={limitOptions}
-                  defaultValue={'10'}
-                />
+              <Dropdown
+                inline={true}
+                options={limitOptions}
+                defaultValue={'10'}
+              />
                 Total count: {'200'}
-              </div>
-              <Table striped singleLine columns={11}>
-                <Table.Header>
-                  <Table.Row>
-                    <Table.HeaderCell>Date & Time</Table.HeaderCell>
-                    <Table.HeaderCell>Dispense Type</Table.HeaderCell>
-                    <Table.HeaderCell>Patient Number</Table.HeaderCell>
-                    <Table.HeaderCell>Medication</Table.HeaderCell>
-                    <Table.HeaderCell>LotId</Table.HeaderCell>
-                    <Table.HeaderCell>Quantity</Table.HeaderCell>
-                    <Table.HeaderCell>Dispensed by</Table.HeaderCell>
-                    <Table.HeaderCell>Information</Table.HeaderCell>
-                  </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                  {
-                    props.historicals.map(history => <DispenseLogRow key={history._id} history={history} />)
-                  }
-                </Table.Body>
-                <Table.Footer>
-                  <Table.Row>
-                    <Table.HeaderCell colSpan="11">
-                      <Pagination totalPages={10} activePage={1}/>
-                    </Table.HeaderCell>
-                  </Table.Row>
-                </Table.Footer>
-              </Table>
-            </Segment>
-          </Grid>
+            </div>
+            <Table striped singleLine columns={11}>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Date & Time</Table.HeaderCell>
+                  <Table.HeaderCell>Dispense Type</Table.HeaderCell>
+                  <Table.HeaderCell>Patient Number</Table.HeaderCell>
+                  <Table.HeaderCell>Medication</Table.HeaderCell>
+                  <Table.HeaderCell>LotId</Table.HeaderCell>
+                  <Table.HeaderCell>Quantity</Table.HeaderCell>
+                  <Table.HeaderCell>Dispensed by</Table.HeaderCell>
+                  <Table.HeaderCell>Information</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {
+                  historicals.map(history => <DispenseLogRow key={history._id} history={history} />)
+                }
+              </Table.Body>
+              <Table.Footer>
+                <Table.Row>
+                  <Table.HeaderCell colSpan="11">
+                    <Pagination totalPages={10} activePage={1}/>
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Footer>
+            </Table>
+          </Segment>
         </Container>
       </div>
     );
