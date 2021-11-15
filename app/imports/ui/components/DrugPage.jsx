d9fea6b02270d8e795d0fc3060f196b0d6a3b0bb
Glen Larita <glarita@hawaii.edu>
2021-11-14 14:35:57 -1000

    Not using uniforms anymore. Still need to have submit update collection

--- app/imports/ui/components/DrugPage.jsx
+++ app/imports/ui/components/DrugPage.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Grid, Button, Segment, Header, Container, GridColumn, Item, ItemGroup, List, ListItem, ItemMeta,
-  ItemContent, ItemDescription, Modal, ListHeader,
+  ItemContent, ItemDescription, Modal, ListHeader, Form,
 } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { AutoForm, ErrorsField, SubmitField, LongTextField, TextField } from 'uniforms-semantic';
+// import { AutoForm, ErrorsField, SubmitField, LongTextField } from 'uniforms-semantic';
 import { withRouter } from 'react-router-dom';
 import swal from 'sweetalert';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+// import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+// import SimpleSchema from 'simpl-schema';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 import { Medications } from '../../api/medication/MedicationCollection';
 import { removeItMethod, updateMethod } from '../../api/base/BaseCollection.methods';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
-const bridge = new SimpleSchema2Bridge(Medications._schema);
-
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const DrugPage = ({ info, lotId, brand, expire, quantity, note, donated, locate }) => {
+  const [noteField, setNoteField] = useState({
+    note: note,
+  });
   const [open, setOpen] = React.useState(false);
   const [secondOpen, setSecondOpen] = React.useState(false);
   const notes = {
@@ -27,6 +29,8 @@ const DrugPage = ({ info, lotId, brand, expire, quantity, note, donated, locate 
     marginRight: '110px',
   };
 
+  // const identifier = info.lotIds.find(obj => obj.lotId === lotId);
+
   const font1 = {
     fontSize: '16px',
   };
@@ -35,8 +39,17 @@ const DrugPage = ({ info, lotId, brand, expire, quantity, note, donated, locate 
     fontSize: '15px',
   };
 
+  const handleChange = (event) => {
+    setNoteField(event.target.value);
+  };
+
   const submit = (data) => {
-    const updateData = { id: data.med.drug, lotId };
+    const exists = Medications.findDoc(info._id);
+    const { lotIds } = exists;
+    const target = lotIds.find(obj => obj.lotId === lotId);
+    target.note = data;
+    console.log(target.note);
+    const updateData = { id: exists, lotIds };
     const collectionName = Medications.getCollectionName();
     updateMethod.callPromise({ collectionName, updateData })
       .catch(error => swal('Error', error.message, 'error'))
@@ -96,7 +109,7 @@ const DrugPage = ({ info, lotId, brand, expire, quantity, note, donated, locate 
                         <ListItem><ListHeader>Quantity in Stock</ListHeader>{quantity}</ListItem>
                         <ListItem><ListHeader>Location</ListHeader>{locate}</ListItem>
                         <ListItem><ListHeader>tabs or mL</ListHeader>{info.unit}</ListItem>
-                        <ListItem><ListHeader>Donated?</ListHeader>         {
+                        <ListItem><ListHeader>Donated?</ListHeader> {
                           donated ?
                             'Item donated'
                             :
@@ -158,12 +171,11 @@ const DrugPage = ({ info, lotId, brand, expire, quantity, note, donated, locate 
               <Grid.Column>
                 <Header as="h3">{info.drug}</Header>
                 <Header as="h4">Lot Number: {lotId}</Header>
-                <AutoForm schema={bridge} onSubmit={data => submit(data)} model={Medications.findDoc(info._id)}>
-                  <LongTextField name={'lotIds.$.note'}/>
-                  <TextField name='unit'/>
-                  <SubmitField value='Submit'/>
-                  <ErrorsField />
-                </AutoForm>
+                <Form>
+                  <Form.TextArea label='Additional Notes' name='note' onChange={handleChange} defaultValue={noteField.note}
+                    id={COMPONENT_IDS.ADD_MEDICATION_NOTES}/>
+                </Form>
+                <Button onClick={() => submit(noteField)}>Submit</Button>
               </Grid.Column>
             </Grid>
           </Modal.Description>
@@ -192,7 +204,6 @@ DrugPage.propTypes = {
   note: PropTypes.string,
   locate: PropTypes.string,
   donated: PropTypes.bool,
-
 };
 
 // Wrap this component in withRouter since we use the <Link> React Router element.
