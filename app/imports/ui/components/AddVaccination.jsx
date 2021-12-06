5b4a57ca56cab81770ef3f3936b078d60ff55e49
alyssandra-cabading <alyssand@hawaii.edu>
2021-12-05 23:34:13 -1000

    Update AddVaccination.jsx

--- app/imports/ui/components/AddVaccination.jsx
+++ app/imports/ui/components/AddVaccination.jsx
@@ -6,11 +6,13 @@ import PropTypes from 'prop-types';
 import { Sites } from '../../api/site/SiteCollection';
 import { Locations } from '../../api/location/LocationCollection';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
-
+import { Vaccinations } from '../../api/vaccination/VaccinationCollection';
+import { DrugTypes } from '../../api/drugType/DrugTypeCollection';
+import { distinct, getOptions, nestedDistinct } from '../utilities/Functions';
 /** On submit, insert the data. */
 
 /** Renders the Page for Dispensing Inventory. */
-const AddVaccination = (props) => {
+const AddVaccination = ({ ready, vaccines, locations, lotIds, brands }) => {
   const [fields, setFields] = useState({
     site: '',
     newSite: '',
@@ -24,23 +26,30 @@ const AddVaccination = (props) => {
     donated: false,
     note: '',
   });
-  const handleChange = (event, { name, value }) => {
-    setFields({ ...fields, [name]: value });
-  /* const [filteredDrugs, setFilteredDrugs] = useState([]);
+
+  /*
+  const [filteredVaccines, setFilteredVaccines] = useState([]);
   useEffect(() => {
-    setFilteredDrugs(drugs);
-  }, [drugs]);
+    setFilteredDrugs(vaccines);
+  }, [vaccines]);
 
   const [filteredBrands, setFilteredBrands] = useState([]);
   useEffect(() => {
     setFilteredBrands(brands);
   }, [brands]);
-
+*/
   const handleChange = (event, { name, value, checked }) => {
-    setFields({ ...fields, [name]: value !== undefined ? value : checked }); */
+    setFields({ ...fields, [name]: value !== undefined ? value : checked });
   };
 
-  if (props.ready) {
+  const handleCheck = (event, { name, checked }) => {
+    if (!checked) {
+      setFields({ ...fields, [name]: checked, donatedBy: '' });
+    } else {
+      setFields({ ...fields, [name]: checked });
+    }
+  };
+  if (ready) {
     return (
       <Tab.Pane id={COMPONENT_IDS.ADD_FORM}>
         <Header as="h2">
@@ -56,8 +65,8 @@ const AddVaccination = (props) => {
             <Grid.Row>
               <Grid.Column>
                 <Form.Select clearable search label='Vaccine Name'
-                  placeholder=" J&J COVID" name='vaccine'
-                  value={fields.drug} allowAdditions/>
+                  placeholder=" J&J COVID" name='vaccine' options={getOptions(vaccines)}
+                  value={fields.vaccine} allowAdditions/>
               </Grid.Column>
               <Grid.Column className='filler-column' />
               <Grid.Column className='filler-column' />
@@ -66,38 +75,49 @@ const AddVaccination = (props) => {
               {/* TODO: expand drug type column */}
               <Grid.Column>
                 <Form.Select clearable search label='Manufacturer'
-                  placeholder="ACAM2000 Sanofi Pasteur"
+                  placeholder="ACAM2000 Sanofi Pasteur" options={getOptions(brands)}
                   name='brand' onChange={handleChange}/>
               </Grid.Column>
               <Grid.Column>
                 <Form.Select clearable search label='Lot Number'
-                  placeholder="Z9Z99" name='lotId'
+                  placeholder="Z9Z99" name='lotId' options={getOptions(lotIds)}
                   value={fields.lotId} allowAdditions/>
               </Grid.Column>
-              <Grid.Column>
-                <Form.Input label='Minimum Quantity' type='number' min={1} name='minQuantity' className='quantity'
-                  onChange={handleChange} value={fields.minQuantity} placeholder="100"/>
-              </Grid.Column>
-              <Grid.Column className='filler-column' />
-            </Grid.Row>
-            <Grid.Row>
               <Grid.Column>
                 {/* expiration date may be null */}
                 <Form.Input type='date' label='Expiration Date' name='expire'
                   onChange={handleChange} value={fields.expire}/>
               </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <Form.Input label='Minimum Quantity' type='number' min={1} name='minQuantity' className='quantity'
+                  onChange={handleChange} value={fields.minQuantity} placeholder="100"/>
+              </Grid.Column>
               <Grid.Column>
                 <Form.Select compact clearable search label='Location'
-                  placeholder="Case 2" name='location'
+                  placeholder="Case 2" name='location' options={getOptions(locations)}
                   onChange={handleChange} value={fields.location} id={COMPONENT_IDS.ADD_MEDICATION_LOCATION}/>
               </Grid.Column>
               <Grid.Column>
                 <Form.Input label='Quantity' type='number' min={1} name='quantity'
                   onChange={handleChange} value={fields.quantity} placeholder="200"/>
               </Grid.Column>
-              <Grid.Column className='checkbox-column'>
-                <Form.Checkbox label='Donated' name='donated' onChange={handleChange} checked={fields.donated}/>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <Form.Field>
+                  <label>Donated</label>
+                  <Form.Group>
+                    <Form.Checkbox name='donated' className='donated-field'
+                      onChange={handleCheck} checked={fields.donated}/>
+                    <Form.Input name='donatedBy' className='donated-by-field' placeholder='Donated By'
+                      onChange={handleChange} value={fields.donatedBy} disabled={!fields.donated} />
+                  </Form.Group>
+                </Form.Field>
               </Grid.Column>
+              <Grid.Column className='filler-column' />
+              <Grid.Column className='filler-column' />
             </Grid.Row>
             <Grid.Row>
               <Grid.Column>
@@ -122,8 +142,8 @@ const AddVaccination = (props) => {
 AddVaccination.propTypes = {
   currentUser: PropTypes.object,
   sites: PropTypes.array.isRequired,
-  drugs: PropTypes.array.isRequired,
-  // drugTypes: PropTypes.array.isRequired,
+  vaccines: PropTypes.array.isRequired,
+  drugTypes: PropTypes.array.isRequired,
   lotIds: PropTypes.array.isRequired,
   locations: PropTypes.array.isRequired,
   brands: PropTypes.array.isRequired,
@@ -133,21 +153,18 @@ AddVaccination.propTypes = {
 // Currently vaccination subscribes to same drugType collection as medication collection.
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
-  const siteSub = Sites.subscribeSite();
-  // const typeSub = DrugTypes.subscribeDrugType();
+  const typeSub = DrugTypes.subscribeDrugType();
   const locationSub = Locations.subscribeLocation();
-  // const vacSub = Vaccinations.subscribeVaccination();
+  const vacSub = Vaccinations.subscribeVaccination();
   return {
     currentUser: Meteor.user(),
     sites: Sites.find({}).fetch(),
-    locations: Locations.find({}).fetch(),
-    ready: siteSub.ready() && locationSub.ready(),
-    /* drugs: distinct('drug', Vaccinations),
+    vaccines: distinct('vaccine', Vaccinations),
     drugTypes: distinct('drugType', DrugTypes),
-    lotIds: distinct('lotId', Vaccinations),
+    lotIds: nestedDistinct('lotId', Vaccinations),
     locations: distinct('location', Locations),
     brands: distinct('brand', Vaccinations),
-    ready: typeSub.ready() && locationSub.ready() && vacSub.ready(), */
+    ready: typeSub.ready() && locationSub.ready() && vacSub.ready(),
 
   };
 })(AddVaccination);
